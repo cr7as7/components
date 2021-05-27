c0b50da854de36e981a448f0db0f710d1040fd8e
Amber Singh <ambersinghrok@gmail.com>
2021-05-27 12:12:39 +0530

    props used

--- src/index.js
+++ src/index.js
@@ -6,12 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
 <div className="ui container comments">
-  <CommentDetail />
-  <CommentDetail />
-  <CommentDetail />
-  <CommentDetail />
-
-  
+  <CommentDetail 
+   author="Amber" 
+   timeAgo="Today at 4:45PM"
+   comment="I m the best "
+   avatar={faker.image.avatar()}
+   />
+  <CommentDetail 
+   author="Akash" 
+   timeAgo="Today at 2:00AM" 
+   comment="okay ,but I strongly disagree "
+   avatar={faker.image.avatar()}
+   />
+  <CommentDetail 
+   author="Inu" 
+   timeAgo="Yeasteday at 5:00PM"
+   comment="poha mast cheez hai"
+   avatar={faker.image.avatar()}
+   />
 </div>
   );
 }
