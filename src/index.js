00dbd5815b0e267d485c9a912f51a46efc11602a
Amber Singh <ambersinghrok@gmail.com>
2021-05-27 13:48:38 +0530

    approval card

--- src/index.js
+++ src/index.js
@@ -2,28 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
 <div className="ui container comments">
-  <CommentDetail 
+  <ApprovalCard >
+   <CommentDetail 
    author="Amber" 
    timeAgo="Today at 4:45PM"
    comment="I m the best "
    avatar={faker.image.avatar()}
    />
-  <CommentDetail 
+  </ApprovalCard>
+  <ApprovalCard>
+   <CommentDetail 
    author="Akash" 
    timeAgo="Today at 2:00AM" 
    comment="okay ,but I strongly disagree "
    avatar={faker.image.avatar()}
    />
-  <CommentDetail 
+  </ApprovalCard>
+  <ApprovalCard>
+   <CommentDetail 
    author="Inu" 
    timeAgo="Yeasteday at 5:00PM"
    comment="poha mast cheez hai"
    avatar={faker.image.avatar()}
    />
+  </ApprovalCard>
+  
 </div>
   );
 }
